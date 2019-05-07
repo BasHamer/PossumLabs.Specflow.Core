489074ef071524b1ab61a2237a380ab165fa5806
Bas Hamer <github@bashamer.com>
2019-05-07 07:49:26 -0600

    concurrency fix by using build in classes

--- PossumLabs.Specflow.Core/CachedTypeAccessor.cs
+++ PossumLabs.Specflow.Core/CachedTypeAccessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
@@ -9,67 +10,26 @@ namespace PossumLabs.Specflow.Core
     {
         static CachedTypeAccessor()
         {
-            Properties = new Dictionary<Type, PropertyInfo[]>();
-            Fields = new Dictionary<Type, FieldInfo[]>();
-            Methods = new Dictionary<Type, MethodInfo[]>();
-            Constructors = new Dictionary<Type, ConstructorInfo[]>();
+            Properties = new ConcurrentDictionary<Type, PropertyInfo[]>();
+            Fields = new ConcurrentDictionary<Type, FieldInfo[]>();
+            Methods = new ConcurrentDictionary<Type, MethodInfo[]>();
+            Constructors = new ConcurrentDictionary<Type, ConstructorInfo[]>();
         }
 
-        private static Dictionary<Type, PropertyInfo[]> Properties { get; }
-
+        private static ConcurrentDictionary<Type, PropertyInfo[]> Properties { get; }
         public static PropertyInfo[] CachedGetProperties(this Type t)
-        {
-            if (Properties.ContainsKey(t))
-                return Properties[t];
-            lock(Properties)
-            {
-                if (!Properties.ContainsKey(t))
-                    Properties.Add(t,t.GetProperties(BindingFlags.Public | BindingFlags.Instance));
-            }
-            return Properties[t];
-        }
+            =>Properties.GetOrAdd(t, t.GetProperties(BindingFlags.Public | BindingFlags.Instance));
 
-        private static Dictionary<Type, FieldInfo[]> Fields { get; }
-
+        private static ConcurrentDictionary<Type, FieldInfo[]> Fields { get; }
         public static FieldInfo[] CachedGetFields(this Type t)
-        {
-            if (Fields.ContainsKey(t))
-                return Fields[t];
-            lock (Fields)
-            {
-                if (!Fields.ContainsKey(t))
-                    Fields.Add(t, t.GetFields(BindingFlags.Public | BindingFlags.Instance));
-            }
-            return Fields[t];
-        }
+            => Fields.GetOrAdd(t, t.GetFields(BindingFlags.Public | BindingFlags.Instance));
 
-        private static Dictionary<Type, MethodInfo[]> Methods { get; }
-
+        private static ConcurrentDictionary<Type, MethodInfo[]> Methods { get; }
         public static MethodInfo[] CachedGetMethods(this Type t)
-        {
-            if (Methods.ContainsKey(t))
-                return Methods[t];
-            lock (Methods)
-            {
-                if (!Methods.ContainsKey(t))
-                    Methods.Add(t, t.GetMethods());
-            }
-            return Methods[t];
-        }
+            => Methods.GetOrAdd(t, t.GetMethods());
 
-        private static Dictionary<Type, ConstructorInfo[]> Constructors { get; }
-
+        private static ConcurrentDictionary<Type, ConstructorInfo[]> Constructors { get; }
         public static ConstructorInfo[] CachedGetConstructors(this Type t)
-        {
-            if (Constructors.ContainsKey(t))
-                return Constructors[t];
-            lock (Constructors)
-            {
-                if (!Constructors.ContainsKey(t))
-                    Constructors.Add(t, t.GetConstructors(BindingFlags.Public | BindingFlags.Instance));
-            }
-            return Constructors[t];
-        }
-        
+            => Constructors.GetOrAdd(t, t.GetConstructors(BindingFlags.Public | BindingFlags.Instance));
     }
 }
