a6a01461053bf08d5b91cf0a01cd4698e1470fdf
Bas Hamer <github@bashamer.com>
2019-05-28 20:59:47 -0600

    possum lab additions merged in

---
+++ PossumLabs.Specflow.Core/PossumLab/IRequireKeepAlive.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PossumLabs.Specflow.Core.PossumLab
+{
+    public interface IRequireKeepAlive
+    {
+        int KeepAlivePeriodInMs { get; }
+        Action KeepAlive { get; }
+    }
+}
